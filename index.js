8a6e98636745cb204cb46239467a4ef03d901c5b
Sukumar Reddy B <sukumar1118@gmail.com>
2024-08-25 12:41:49 +0530

    JS start

---
+++ index.js
@@ -0,0 +1,11 @@
+const message = 'Hello world'; // Try edit me
+
+var x = 7;
+function getName() {
+  console.log('Namate JS');
+}
+getName();
+console.log(x);
+
+// Log to console
+console.log(message);
