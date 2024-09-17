d20f43018c9060098ce7e9cd4fa453fd8669f92a
Sukumar Reddy B <sukumar1118@gmail.com>
2024-09-17 22:42:19 +0530

    promises

--- JS Concepts/promiseProducer.js
+++ JS Concepts/promiseProducer.js
@@ -1,23 +1,26 @@
 /*
     Create a Promise: 
-        -> create a promise object using promise constructor function.
+        -> create a promise using promise constructor function.
 
-        -> It takes function as argument with resolve and reject function params.
+        -> It takes function as argument with resolve and reject function parameters.
 
-        -> you can Resolve or Reject the promise as per your choice based on the result.
+        -> You can Resolve or Reject the promise as per your choice based on the result.
 
-        -> After catch(), remain .then() functions will execute and chain will ot break.
+        -> After catch(), remain .then() functions will execute and chain will not break.
 
-        -> catch() can be added in the middle for some .then() functions in the middle of 
+        -> .then() fucntions after the error(or rejection) and before catch will not 
+            be executed and skipped.
+
+        -> catch() can be added for some .then() functions in the middle of 
             the chain to handle errors for that portion of functions and you can add 
             catch() at the end for remaining functions.
 */
 
 const cart = ["books", "shoes", "t-shirts"];
 
-const promise = createOrder(cart);
+// const promise = createOrder(cart);
 
-promise
+createOrder(cart)
   .then(function (orderId) {
     console.log("Order created:", orderId);
   })
@@ -52,7 +55,6 @@ function createOrder(cart) {
       }, 3000);
     }
   });
-
   return pr;
 }
 
